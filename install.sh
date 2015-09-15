da1a84ae5ffff8d1975bbfa1189686378b7f839c
Tim Gremalm <tim@gremalm.se>
2015-09-15 22:12:36 +0200

    Added support for offset and scaling factor
    Also removed som unnecesary stuff

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 chmod +x *.sh *.py
-sudo patch -b /boot/config.txt 7inch.patch
 sudo apt-get install -y python3-pip libudev-dev
-sudo pip-3.2 install python-uinput pyudev
-#if pip-3.2 can't be found, please use pip3
-#sudo pip3 install python-uinput pyudev
+sudo pip3 install python-uinput pyudev
 
 sudo cp touch.py /usr/bin/
 sudo cp touch.sh /etc/init.d/
